1d88f21a70132bdd977b427ffe6132243c44a8b0
viniciushetzel <viniciuscarbonario@gmail.com>
2021-10-17 15:33:15 -0400

    Add novos arquivos

---
+++ scripts/contato.js
@@ -0,0 +1,34 @@
+// Máscara para o telefone celular, 
+
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
+function mCEL(tel) {
+    tel=tel.replace(/\D/g,"")
+    tel=tel.replace(/^(\d)/,"($1")
+    tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+    if(tel.length == 9) {
+        tel=tel.replace(/(.{1})$/,"-$1")
+    } else if (tel.length == 10) {
+        tel=tel.replace(/(.{2})$/,"-$1")
+    } else if (tel.length == 11) {
+        tel=tel.replace(/(.{3})$/,"-$1")
+    } else if (tel.length == 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    } else if (tel.length > 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    }
+    return tel;
+}
+
+
+
+// Caso todos os campos sejam preenchidos, é aceito enviar
+function sucesso() {
+    window.alert("Os seus dados foram enviados com sucesso!");
+}
